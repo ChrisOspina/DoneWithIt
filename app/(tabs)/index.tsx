693bb0454d1b8f81be5efa06a368aafdbb08c3ac
ChrisOspina <ChrisOspina99@gmail.com>
2025-07-18 15:44:27 -0400

    Learning about flexbox

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,81 @@
-import { Alert, Button, Image, StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View
+} from 'react-native';
+
+import { useDeviceOrientation } from '@react-native-community/hooks';
 
 
 export default function HomeScreen() {
   console.log('App Executed');
 
+  const orientation = useDeviceOrientation()
+
+  //Just to output the dimension
+  //console.log(Dimensions.get('screen'))
+
+  //console.log(useWindowDimensions());
+  console.log(orientation);
+
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Text>Hello React Native</Text>
-      <View style={{width:200, height:70, backgroundColor:"dodgerblue" }}/>
-      <TouchableNativeFeedback onPress={()=>console.log("Image pressed")}>
-      <Image
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300"}}/>
-      </TouchableNativeFeedback>
-       <Button
-          color= "orange"
-          title='Click Me'
-          onPress={()=> Alert.alert("Test Alert", "Button clicked",[
-            {text: "Yes", onPress:()=> console.log("Yes")},
-            {text: "No", onPress:()=> console.log}
-          ])}>
-      </Button>
-    </SafeAreaView>
+      <View style={
+          {
+            backgroundColor:"#fff",
+            flex:1,
+            flexDirection: "row", //horizontal
+            justifyContent: "center", //main axis
+            alignItems:"stretch" //secondary
+            
+          }
+        }>
+        <View style={{
+            width: 100,
+            height: 300,
+            backgroundColor:"dodgerblue"
+          }}/>
+
+        <View style={{
+          backgroundColor:"gold",
+          flex: 1,
+          width: 100,
+          height: 200,
+        }}/>
+        <View style={{
+              backgroundColor:"tomato",
+              flex: 1,
+              width: 100,
+              height: 100,
+            }}/>
+      </View>
+//       <View>
+//              <TouchableNativeFeedback onPress={()=>console.log("Image pressed")}>
+//                   <Image
+//                     fadeDuration={1000}
+//                     source={{
+//                       width: 200,
+//                       height: 300,
+//                       uri: "https://picsum.photos/200/300"}}/>
+//                   </TouchableNativeFeedback>
+//        <Button
+//           color= "orange"
+//           title='Click Me'
+//           onPress={()=> Alert.alert("Test Alert", "Button clicked",[
+//             {text: "Yes", onPress:()=> console.log("Yes")},
+//             {text: "No", onPress:()=> console.log}
+//           ])}>
+//       </Button>
+//       </View>
   );
 }
 
-const containerStyle = {backgroundColor:"orange"};
+//const containerStyle = {backgroundColor:"orange"};
 
 const styles = StyleSheet.create({
   container:{
     flex:1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight: 0
   }
 });
