fb66ec33f15c8a25ebd2b481e78eb035d139cab7
ChrisOspina <ChrisOspina99@gmail.com>
2025-07-21 16:44:41 -0400

    Completely refactored app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,95 +1,13 @@
-import {
-  Platform,
-  StatusBar,
-  StyleSheet,
-  View
-} from 'react-native';
-
-import { useDeviceOrientation } from '@react-native-community/hooks';
+import React from 'react';
+import ViewImageScreen from "../screens/ViewImageScreen";
 
 
-export default function HomeScreen() {
-  console.log('App Executed');
 
-  const orientation = useDeviceOrientation()
-
-  //Just to output the dimension
-  //console.log(Dimensions.get('screen'))
-
-  //console.log(useWindowDimensions());
-  console.log(orientation);
+export default function App() {
 
   return (
-      <View style={
-          {
-            backgroundColor:"#fff",
-            flex:1,
-            flexDirection: "row", //horizontal
-            justifyContent: "center", //main axis
-            alignItems:"stretch", //secondary
-            alignContent: "center",
-            flexWrap: "wrap"
-            
-          }
-        }>
-        <View style={{
-            height: 300,
-            backgroundColor:"dodgerblue",
-            flexBasis: 100 //width
-          }}/>
+      <ViewImageScreen/>
 
-        <View style={{
-          backgroundColor:"gold",
-          flex: 1,
-          width: 100,
-          height: 200,
-        }}/>
-        <View style={{
-              backgroundColor:"tomato",
-              flex: 1,
-              width: 100,
-              height: 100,
-            }}/>
-          <View style={{
-              backgroundColor:"grey",
-              flex: 1,
-              width: 100,
-              height: 100,
-            }}/>
-          <View style={{
-              backgroundColor:"greenyellow",
-              flex: 1,
-              width: 100,
-              height: 100,
-            }}/>
-      </View>
-//       <View>
-//              <TouchableNativeFeedback onPress={()=>console.log("Image pressed")}>
-//                   <Image
-//                     fadeDuration={1000}
-//                     source={{
-//                       width: 200,
-//                       height: 300,
-//                       uri: "https://picsum.photos/200/300"}}/>
-//                   </TouchableNativeFeedback>
-//        <Button
-//           color= "orange"
-//           title='Click Me'
-//           onPress={()=> Alert.alert("Test Alert", "Button clicked",[
-//             {text: "Yes", onPress:()=> console.log("Yes")},
-//             {text: "No", onPress:()=> console.log}
-//           ])}>
-//       </Button>
-//       </View>
   );
 }
 
-//const containerStyle = {backgroundColor:"orange"};
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight: 0
-  }
-});
